f1744e8144b4b5652adfc2a2cc9f6e29882fd437
MSI\91990 <rnair1607@gmail.com>
2023-07-23 13:24:38 +0100

    debug

--- Code/train.py
+++ Code/train.py
@@ -298,8 +298,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
 
     while epoch < args.num_epochs:
-        if args.distributed:
-            dataloader.train_sampler.set_epoch(epoch)
+        # if args.distributed:
+        #     dataloader.train_sampler.set_epoch(epoch)
 
         for step, sample_batched in enumerate(dataloader.data):
             optimizer.zero_grad()

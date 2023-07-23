7f64437d916c27117fe331801bffddf145f722d5
MSI\91990 <rnair1607@gmail.com>
2023-07-23 12:43:58 +0100

    debug

--- Code/train.py
+++ Code/train.py
@@ -156,6 +156,7 @@ def main_worker(gpu, ngpus_per_node, args):
     
     dataloader = DataLoader(args, 'train')
     print("Loaded Data loader")
+    print(dir(dataloader.data))
     print(dataloader.data.__getattribute__('le'))
     
 

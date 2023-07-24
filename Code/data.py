b4cda3edde7cbe73445e4475f7d3535fe81d76f2
MSI\91990 <rnair1607@gmail.com>
2023-07-24 15:33:41 +0100

    debug data loader

--- Code/data.py
+++ Code/data.py
@@ -28,14 +28,14 @@ class AcaDataLoader(object):
     def __init__(self, args, mode):
         if mode == 'train':
             self.training_samples = DataLoadPreprocess(args, mode, transform=preprocessing_transforms(mode))
-            # if args.distributed:
-            #     self.train_sampler = torch.utils.data.distributed.DistributedSampler(self.training_samples)
-            # else:
-            #     self.train_sampler = None
+            if args.distributed:
+                self.train_sampler = torch.utils.data.distributed.DistributedSampler(self.training_samples)
+            else:
+                self.train_sampler = None
     
             self.data = DataLoader(self.training_samples, args.batch_size,
                                    shuffle=True,
-                                #    num_workers=args.num_threads,
+                                   num_workers=args.num_threads,
                                    pin_memory=True,
                                    sampler= None
                                    )

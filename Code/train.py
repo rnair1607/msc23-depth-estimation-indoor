4e9087b55731bc8ac6db40d8437b4221e86e24ce
MSI\91990 <rnair1607@gmail.com>
2023-07-24 15:30:41 +0100

    debug

--- Code/train.py
+++ Code/train.py
@@ -253,12 +253,12 @@ def main_worker(gpu, ngpus_per_node, args):
             torch.cuda.set_device(args.gpu)
             model.cuda(args.gpu)
             args.batch_size = int(args.batch_size / ngpus_per_node)
-            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+            # model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
         else:
             model.cuda()
-            model = torch.nn.parallel.DistributedDataParallel(model, find_unused_parameters=True)
+            # model = torch.nn.parallel.DistributedDataParallel(model, find_unused_parameters=True)
     else:
-        model = torch.nn.DataParallel(model)
+        # model = torch.nn.DataParallel(model)
         model.cuda()
 
     if args.distributed:
